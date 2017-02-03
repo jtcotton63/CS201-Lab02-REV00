22de580bb3b14332e9f9e68351aa29fa8b867b4f
Joseph Cotton <jtcotton63@gmail.com>
2017-02-02 23:07:35 -0700

    Weather functions

--- js/weather.js
+++ js/weather.js
@@ -1,34 +1,34 @@
-function OnCityInput() {
+function WeatherOnCityInput() {
 
 	// http://stackoverflow.com/questions/15764844/jquery-getjson-save-result-into-variable
-	var searchTerm = $( "#cityField" ).val();
+	var searchTerm = $("#cityField").val();
 	var autoURL = "http://bioresearch.byu.edu/cs260/jquery/getcity.cgi?q=";
 	autoURL += searchTerm;
 
-	if( searchTerm === "")
+	if(searchTerm === "")
 		return;
 
-	$.getJSON( autoURL, function( data ) {
+	$.getJSON(autoURL, function(data) {
 		var resultsAsString = "";
-		$.each( data, function( i , item ) {
+		$.each(data, function(i, item) {
 
 			// http://stackoverflow.com/questions/441018/replacing-spaces-with-underscores-in-javascript
-			var temp = data[ i ].city;
+			var temp = data[i].city;
 			if(typeof temp === 'undefined') {
 				return;
 			}
 
-			temp = temp.replace( / /g,"_" );
+			temp = temp.replace(/ /g,"_");
 
 			resultsAsString += "<li "
-			+ "onclick=OnClickEntry(\"" + temp + "\") "
+			+ "onclick=WeatherOnClickEntry(\"" + temp + "\") "
 			+ "style=\"cursor: pointer\""
 			+ ">" 
 			+ data[ i ].city;
 			+ "</li>";
 		});
 
-		$( "#txtHint" ).html(resultsAsString);
+		$("#txtHint").html(resultsAsString);
 
 	});
 
@@ -36,17 +36,14 @@ function OnCityInput() {
 
 
 
-function OnClickEntry( listValue ) {
-
-	listValue = listValue.replace( /_/g, " " );
-
-	document.getElementById( "cityfield" ).value = listValue;
-
-	OnCityInput();
+function WeatherOnClickEntry(listValue) {
+	listValue = listValue.replace(/_/g," ");
+	document.getElementById("cityField").value = listValue;
+	WeatherOnCityInput();
 };
 
-function OnClickSubmit() {
-	var cityName = $( "#cityfield" ).val();
+function WeatherOnClickSubmit() {
+	var cityName = $("#cityField").val();
 
 	// Get detailed weather information for city
 	var myURL = "https://api.wunderground.com/api/6241d9cf04b40a82/geolookup/conditions/q/UT/" + cityName + ".json";
@@ -54,14 +51,14 @@ function OnClickSubmit() {
 	$.ajax({
 		url : myURL,
 		dataType : 'jsonp',
-		success : function( data ) {
+		success : function(data) {
 			var weather = data.current_observation.weather;
 			var temperature = data.current_observation.temperature_string;
 
 
 			// Populate the text box
 			// http://stackoverflow.com/questions/415602/set-value-of-textarea-in-jquery
-			$( "#txtarea" ).val( cityName );
+			$("#txtarea").val(cityName);
 
 			var weatherDivString = "<br>";
 			weatherDivString += "Current weather prediction for " + cityName + ":";
@@ -73,7 +70,7 @@ function OnClickSubmit() {
 			weatherDivString += temperature;
 			weatherDivString += "<br>";
 
-			$( "#weather-info" ).html( weatherDivString );
+			$("#weather-info").html(weatherDivString);
 		}
 	});
 };
